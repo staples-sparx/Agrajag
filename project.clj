57b0a7129e84fce573fc1662414ee6908de9f506
Srihari Sriraman and Udit Kumar <pair+srihari+udit@nilenso.com>
2016-08-24 17:50:25 +0530

    Use `repmgr cluster show`, not the database view.

--- project.clj
+++ project.clj
@@ -5,8 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [zookeeper-clj "0.9.4"]
-                 [org.clojure/java.jdbc "0.6.2-alpha2"]
-                 [org.clojure/tools.logging "0.3.1"]
-                 [org.postgresql/postgresql "9.4.1208"]]
+                 [org.clojure/tools.logging "0.3.1"]]
   :plugins [[cider/cider-nrepl "0.13.0"]
             [refactor-nrepl "2.2.0"]])
--- src/repmgr_to_zk/repmgr.clj
+++ src/repmgr_to_zk/repmgr.clj
@@ -1,33 +1,23 @@
 (ns repmgr-to-zk.repmgr
-  (:require [clojure.java.jdbc :as j]
+  (:require [clojure.java.shell :as shell]
             [clojure.string :as s]
-            [clojure.tools.logging :as log]
-            [repmgr-to-zk.config :as config]
-            [clojure.java.shell :as shell])
-  (:import [java.sql SQLException]))
+            [repmgr-to-zk.config :as config]))
 
-(def default-db-config
-  {:dbtype "postgresql"
-   :dbname "repmgr"
-   :host "localhost"
-   :user "repmgr"
-   :password "repmgr"
-   :port "5432"})
+(defn parse-output [cluster-show-output]
+  (for [line (drop 2 (s/split cluster-show-output #"\n"))
+        :let [[role node-name upstream connection-string] (map s/trim (s/split line #"\|"))]]
+    {:name node-name
+     :role (re-find #"\w+" role)
+     :upstream upstream
+     :connection-string connection-string}))
 
-(def db
-  (merge default-db-config (config/lookup :repmgr)))
+(defn cluster-status []
+  (let [config-file (config/lookup :repmgr :config-file)
+        cluster-show-output (:out (shell/sh "repmgr" "-f" config-file "cluster" "show"))]
+    (parse-output cluster-show-output)))
 
-(defn read-cluster-status []
-  (try
-    (let [suffix (config/lookup :repmgr :suffix)
-          query (format "SELECT conninfo, type, name, upstream_node_name, id
-                       FROM repmgr_%s.repl_show_nodes;"
-                        suffix)]
-      (j/query db query))
-    (catch SQLException e
-      (log/error e "Unable to connect to the DB."))))
-
-;; TODO: this is not correct. we should be shelling out to repmgr cluster show --csv.
 (defn master []
-  (let [cluster-status (read-cluster-status)]
-    (:name (first (filter #(= "master" (:type %)) cluster-status)))))
+  (->> (cluster-status)
+       (filter #(= "master" (:role %)))
+       first
+       :name))
