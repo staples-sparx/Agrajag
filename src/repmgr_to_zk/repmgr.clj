4156a2d20a5bd00cd16af9c21906d0ce768723be
udit <yudistrange@gmail.com>
2017-01-20 18:21:30 +0530

    Fixes the repmgr queries to look up env specific tables

--- src/repmgr_to_zk/repmgr.clj
+++ src/repmgr_to_zk/repmgr.clj
@@ -4,6 +4,8 @@
             [repmgr-to-zk.config :as config]
             [repmgr-to-zk.db :as db]))
 
+(def repmgr-env (str "repmgr_" (config/lookup :env)))
+
 (defn- parse-output [cluster-show-output]
   (for [line (drop 2 (s/split cluster-show-output #"\n"))
         :let [[role node-name upstream connection-string] (map s/trim (s/split line #"\|"))]]
@@ -28,17 +30,18 @@
       (throw (ex-info "No master node." nodes)))))
 
 (defn latest-promoted-standby []
-  (let [event "SELECT node_id, event_timestamp
-               FROM repmgr_perf.repl_events
-               WHERE event = 'standby_promote' AND successful = 't'
-               ORDER BY event_timestamp DESC LIMIT 1;"]
+  (let [event (str "SELECT node_id, event_timestamp FROM "
+                   repmgr-env
+                   ".repl_events WHERE event = 'standby_promote'
+                    AND successful = 't'
+                    ORDER BY event_timestamp DESC LIMIT 1;")]
     (db/with-read-only-connection conn
       (first (db/q conn event)))))
 
 (defn node-by-id [^Integer id]
-  (let [event ["SELECT id, name
-               FROM repmgr_perf.repl_nodes
-               WHERE id = ?;" id]]
+  (let [event [(str "SELECT id, name FROM "
+                    repmgr-env
+                    ".repl_nodes WHERE id = ?;") id]]
     (db/with-read-only-connection conn
       (first (db/q conn event)))))
 
